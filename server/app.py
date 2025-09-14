e1267eb98a800531feafc72fa759ce22269fac41
beatricemwenje <you@example.com>
2025-09-14 19:36:21 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -3,17 +3,18 @@
 
 from flask import Flask
 from flask_migrate import Migrate
-
-from models import db, Employee, Onboarding, Review
+from models import db
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
+# Configuration for SQLite DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize extensions
 db.init_app(app)
+migrate = Migrate(app, db)
 
-
-if __name__ == "__main__":
-    app.run(port=5555, debug=True)
+@app.route('/')
+def index():
+    return 'Server is running!'
